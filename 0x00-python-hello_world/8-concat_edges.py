1b07847cbb14e837ddafed18e00ffb07f8ab83b0
Ikeh Victor <victorikeh77@gmail.com>
2024-03-11 12:41:19 -0400

    concat edge

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "object-oriented\
- programming with Python"
-str += str[39:87]
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:111] + " " + str[:6]
 print(str)
