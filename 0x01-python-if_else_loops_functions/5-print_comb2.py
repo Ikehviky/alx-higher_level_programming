0abf1127511fc8ccf322a7cd0059b04833705f89
Ikeh Victor <victorikeh77@gmail.com>
2024-03-13 06:59:50 -0400

    print combination

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{:02}".format(i), end=" ")
+        print("{:02}".format(i))
     else:
         print("{:02},".format(i), end=" ")
