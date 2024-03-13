9bfaf26007690d355fe5100fadd84c77a04bd31e
Ikeh Victor <victorikeh77@gmail.com>
2024-03-13 07:25:39 -0400

    print combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i * 10 + j), end=", " if i != 9 or j != 8 else "")
+        print("{:02d}".format(i * 10 + j), \
+            end=", " if i != 9 or j != 8 else "")
