e7c2d6f08b6f34af734a2cf446b25920519e5273
Ikeh Victor <victorikeh77@gmail.com>
2024-03-20 20:38:02 -0400

    import a funcion

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     a = 1
     b = 2
     from add_0 import add
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
