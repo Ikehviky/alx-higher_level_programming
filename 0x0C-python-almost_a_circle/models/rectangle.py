bd622e87e6749da562ad137ff5c55e2afd53cab2
Ikeh Victor <victorikeh77@gmail.com>
2024-04-24 15:21:51 -0400

    updated rectangle test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,10 @@ class Rectangle(Base):
 
     def display(self):
         """Print the Rectangle using the `#` character."""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Return the string representation of the Rectangle."""
