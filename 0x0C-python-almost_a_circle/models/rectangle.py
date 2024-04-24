166dd22e9cbbfaed03db7a0eb95dde7ddd0d01d0
Ikeh Victor <victorikeh77@gmail.com>
2024-04-24 14:56:27 -0400

    rectangle test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -156,3 +156,4 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
+    
