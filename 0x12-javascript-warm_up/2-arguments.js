883906aa4a70320360b9ecd178c805922bcedd3e
Ikeh Victor <victorikeh77@gmail.com>
2024-06-19 23:09:30 -0400

    2 and 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const args = process.argv.slice(2);
 
 if(args.length === 0){
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const args = process.argv.slice(2);
 
 console.log(args[0] !== undefined ? args[0] : 'no argument');
