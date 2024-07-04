6320acd94f7411888186d358e8a5e196818406fc
Ikeh Victor <victorikeh77@gmail.com>
2024-07-04 07:57:14 -0400

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to that URL, and displays the body of a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | {
-    read status
-    if [ "$status" -eq 200 ]; then
-        curl -s "$1"
-    fi
-}
+curl -sL "$1"
