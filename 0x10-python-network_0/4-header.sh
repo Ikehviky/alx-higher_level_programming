596bec63173a57e2f3b429974d20fa8dcb5fd4b4
Ikeh Victor <victorikeh77@gmail.com>
2024-07-04 09:16:04 -0400

    header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL with X-School-User-Id header and displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+curl -s -H "X-School-User-Id: 98" $1
